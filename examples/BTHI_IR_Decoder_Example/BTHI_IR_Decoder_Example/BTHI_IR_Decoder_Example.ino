4babb141ed6fa465540ccf010138373b03f59916
cmyers <cmyers734@gmail.com>
2013-01-19 00:07:07 -0500

    Added use of new interfaces to example sketch

--- examples/BTHI_IR_Decoder_Example/BTHI_IR_Decoder_Example/BTHI_IR_Decoder_Example.ino
+++ examples/BTHI_IR_Decoder_Example/BTHI_IR_Decoder_Example/BTHI_IR_Decoder_Example.ino
@@ -1,13 +1,20 @@
 #include <BTHI_IR_Decoder.h>
 
-
-void setup(){
-  IR_Decoder.setup();
-  Serial.begin(115200);
-
+void setup() {
+    IR_Decoder.setup();
+    Serial.begin(115200);
 }
 
-void loop(){
-  
+void loop() {
+    int16_t res;
+    ir_decoder_edge_t frame_buffer[64];
+
+    if (IR_Decoder.isFrameAvailable()) {
+        res = IR_Decoder.copyFrame(&frame_buffer[0], sizeof(frame_buffer));
+        if (res > 0) {
+            // Process the frame
+            Serial.println("Frame Received!");
+        }
+    }
 }
 
