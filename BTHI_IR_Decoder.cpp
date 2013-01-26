c8055cba1525436e6740fff08da9611da43403d9
cmyers <cmyers734@gmail.com>
2013-01-25 23:49:35 -0500

    Refined implementation using Timer1 Input Capture.  Refactor to allow for streaming or buffered decode
    Next steps must include:
     * Better documentation
     * More testing
     * Example decode
    
    Could include:
     * Pull buffered decoder out and make it an example instead.
     * Optional noise cancel
     * Adjustable prescaler?
     * Example stream decoder.

--- BTHI_IR_Decoder.cpp
+++ BTHI_IR_Decoder.cpp
@@ -1,169 +1,246 @@
-/*
+/*---------------------------------------------------------------------------
+ * Streaming Infrared Decoder Library
+ * 
+ * This library uses the input capture functionality of Timer 1 on the
+ * atmega328 to do processing of the input waveform from an IR receiver such
+ * as the Vishay TSOP1140.
  *
+ * The use of Timer 1's input capture has the advantages of:
+ *   - overall lower CPU load vs a timer-driven polling approach
+ *   - more reliable than using pulseIn()
+ *   - uses Timer 1's built-in noise canceller
+ *
+ * Disadvantages:
+ *   - Restricted to Pin 8
+ *   - One cannot use PWM channels that are driven by Timer 1
+ *
+ * Another design goal was to allow for both streaming and buffered decoding
+ * approaches.  To accomodate this, there is a class responsible for
+ * interfacing with the input capture hardware, but a delegate is handed each
+ * segment duration.  Once there, it can either decode it on the fly or buffer
+ * it.  A buffered implementation is provided in IR_BufferingStreamDecoder.
+ *
+ * A streaming approach:
+ *    - Can be more complex to implement
+ *    - Uses much less RAM
+ *    - Would likely be designed specifically for a single IR protocol
+ *
+ * A buffered approach:
+ *    - Uses much more RAM
+ *    - May miss frames while processing the last received frame
+ *    - Can be made to evaluate the waveform against many known protocols
  */
-#include "Arduino.h"
+#include <Arduino.h>
 #include "BTHI_IR_Decoder.h"
 
-// We use IR Decoder on Arduino pin 2, which is atmega's PD2
-#define PIN_ATMEGA PIND
-static const int PINBIT_ATMEGA = 2;
-static const int PIN_ARDUINO = 2;
+// We have to instantiate this because we're wiring it manually to the timer1
+// capture and overflow ISRs. The instance needs to be valid.
+IR_HwInterface IR_InputCaptureInterface;
 
-BTHI_IR_Decoder::BTHI_IR_Decoder(void) {
+/**
+ * Setup routine. This routine must be called during your projects setup()
+ * function. It sets up Timer1 to do input capture and chooses an initial
+ * level to capture on based on your polarity setting. It also sets the
+ * specified pin to be an INPUT.
+ *
+ * NOTE: After this call, your PWM's using Timer1 won't work anymore.
+ *
+ * stream_decoder: This delegate will be fed edges and end of frame events.
+ * pin: Must be pin 8 on the Uno. TODO: what about others?
+ * polarity: This determines what the initial edge the input capture unit
+ *    should trigger on. If your receiver is nominally HIGH when there is no
+ *    IR activity, then you should choose IR_POLARITY_HIGH. If it's nominally
+ *    LOW, then choose IR_POLARITY_LOW. If you just don't know yet, you can
+ *    also choose IR_POLARITY_AUTO. This option looks at the level on the line
+ *    and tries to figure it out for you. Probably ok.. but not foolproof.
+ *
+ */
+void IR_HwInterface::setup(IR_StreamDecoder *stream_decoder, 
+        uint8_t pin, ir_polarity_t polarity) {
+    _pin = pin;
+    _decoder = stream_decoder;
+    
+    // Set Initial Timer value
+    TCNT1 = 0;
+    
+    // Put timer 1 into "Normal" mode for input capture
+    TCCR1A = 0;
+
+    if (IR_POLARITY_AUTO == polarity) {
+        // Have to set the pin mode to input early if it's auto to read the
+        // level.  We'll call this again later, but that shouldn't have any
+        // side effects.
+        pinMode(pin, INPUT);
+    
+        if (0 == digitalRead(_pin)) {
+            // First edge is rising
+            TCCR1B |= (1 << ICES1);
+        } else {
+            // First edge is falling
+            TCCR1B &= ~(1 << ICES1);
+        }
+    } else if (IR_POLARITY_LOW == polarity) {
+        // First edge is rising
+        TCCR1B |= (1 << ICES1);
+    } else {
+        // First edge is falling
+        TCCR1B &= ~(1 << ICES1);
+    }
+
+    // Enable input capture interrupts only
+    TIMSK1 = (1 << ICIE1);
+
+    // Noise cancellation on and prescaler /8
+    TCCR1B = (1 << ICNC1) | (1 << CS11);
+
+    pinMode(pin, INPUT);
 }
 
-void BTHI_IR_Decoder::setup(void) {
-    pinMode(PIN_ARDUINO, INPUT);
-    PCICR  |=  4; // enable PCIE2 which services PCINT18
-    PCMSK2 |=  4; // enable PCINT18 --> Pin Change Interrupt of PD2
+IR_HwInterface::IR_HwInterface() {
+}
 
-    // Clear out the frame buffer
-    memset(_frame_buffer, 0, sizeof(_frame_buffer));
-
-    _last_timestamp = 0;
-    _sample_index = 0;
-    _frame_available = 0;
-    _latched_frame_length = 0;
-    _sample_overflows = 0;
-	_frame_overruns = 0;
-
-    _k_dead_time = K_DEADTIME_DEFAULT_US;
-
-    // pin debug to check if we are getting here
-    _pin = 7;
-    pinMode(_pin, OUTPUT);
-    digitalWrite(_pin,LOW);
-    digitalWrite(_pin,HIGH);
-    delay(122); // 
-    digitalWrite(_pin,LOW);
+void IR_HwInterface::overflowInterrupt() {
+    // No more overflow interrupt until it is re-enabled in the capture
+    // interrupt.
+    TIMSK1 &= ~(1<<TOIE1);
+    
+    if (_decoder) {
+        _decoder->endDecode();
+    }
 }
 
 // Interrupt handler
-void BTHI_IR_Decoder::interrupt() {
-    //if (PIN_ATMEGA & (1 << PINBIT_ATMEGA)) return;  // Only consider falling edges
-    uint32_t now;
-    uint32_t elapsed;
-    bool pinState;
+void IR_HwInterface::captureInterrupt() {
+    uint16_t elapsed;
+    uint8_t level;
     
-    now = micros();
-    elapsed = now - _last_timestamp;
+    // Reset TCNT1
+    TCNT1 = 0;
 
-    _last_timestamp = now;
-  
-    // toggle the pin so we know that we are in the ISR again
-    pinState = digitalRead(_pin);
-    digitalWrite(_pin,!pinState);
-    // TODO: Handle overflow
+    elapsed = ICR1;
     
-    if (_sample_index >= sizeof(_frame_buffer)) {
-        if (_sample_overflows < 0xFFFF) {
-            _sample_overflows++;
-        }
+    // Start listening for overflow as well as the input capture interrupt
+    // Make sure to clear the overflow flag, otherwise it will trigger
+    // immediately, giving us a premature end of frame
+    TIFR1 = (1 << TOV1);
+    TIMSK1 = (1<<ICIE1) | (1<<TOIE1);
+
+    level = digitalRead(_pin);
+    
+    if (0 == level) {
+        // Next edge is rising
+        TCCR1B |= (1 << ICES1);
+    } else {
+        // Next edge is falling
+        TCCR1B &= ~(1 << ICES1);
+    }
+
+    // Pass it on to the decode layer
+    if (_decoder) {
+        _decoder->decodeEdge(elapsed);
+    }
+}
+
+IR_BufferingStreamDecoder::IR_BufferingStreamDecoder(void) {
+	ir_segment_t *_segments = NULL;
+	_max_segments = 0;
+	_count = 0;
+	_segment_overflows = 0;
+	_frame_complete = 0;
+    _first_edge = 1;
+}
+
+void IR_BufferingStreamDecoder::debugPrintFrame(void) {
+    Serial.print("Max Segments: ");
+    Serial.println(_max_segments);
+    Serial.print("Segment Count: ");
+    Serial.println(_count);
+    Serial.print("Segment Overflow: ");
+    Serial.println(_segment_overflows);
+
+    for (uint8_t i = 0; i < _count; i++) {
+        Serial.print(i);
+        Serial.print(": ");
+        Serial.println(_segments[i].duration);
+    }
+}
+
+void IR_BufferingStreamDecoder::endDecode(void) {
+    if (_count > 0) {
+        _frame_complete = 1;
+    }
+}
+
+void IR_BufferingStreamDecoder::decodeEdge(uint16_t duration) {
+    // Don't overrun the frame until reset()
+    if (0 != _frame_complete) {
+        return;
     }
     
+    // First edge is ignored because it doesn't complete a segment.
+    // That is, it has no meaningful duration.
+    if (1 == _first_edge) {
+        _first_edge = 0;
+        return;
+    }
+
     // Record the sample at the current index
-    _frame_buffer[_sample_index].level = pinState; // XXX:
-    _frame_buffer[_sample_index].duration = elapsed; // XXX:
-    
-    _sample_index++;
-
-    if (elapsed > _k_dead_time) {
-        // Latch the data and set the flag that new data is available
-        // TODO: Theoretically, you only need to copy as many entries as were 
-        // valid.
-        memcpy(_latched_frame_buffer, _frame_buffer, sizeof(_frame_buffer));
-
-        // Record how many valid samples we took
-        _latched_frame_length = _sample_index;
-
-        // If the frame was already waiting, it means that we missed a frame.
-        // We call this a "frame overrun"
-        if (1 == _frame_available) {
-            if (_frame_overruns < 0xFFFF) {
-                _frame_overruns++;
-            }
+    if (_count >= _max_segments) {
+        if (_segment_overflows < (uint8_t)0xFF) {
+            _segment_overflows++;
         }
-        
-        _frame_available = 1;
-        
-        // Have to reset the sample index since we're starting over with a new
-        // frame
-        _sample_index = 0;
+
+        return;
     }
+
+    _segments[_count++].duration = duration;
 }
 
-uint16_t BTHI_IR_Decoder::getFrameOverrunCount(void) {
-    return _frame_overruns;
-}
-
-uint16_t BTHI_IR_Decoder::clearFrameOverrunCount(void) {
-    uint16_t tmp = _frame_overruns;
-
-    _frame_overruns = 0;
-
-    return tmp;
-}
-
-uint16_t BTHI_IR_Decoder::getSampleOverflowCount(void) {
-    return _sample_overflows;
-}
-
-uint16_t BTHI_IR_Decoder::clearSampleOverflowCount(void) {
-    uint16_t tmp = _sample_overflows;
-
-    _sample_overflows = 0;
-
-    return tmp;
-}
-
-uint8_t BTHI_IR_Decoder::isFrameAvailable(void) {
-    // Check the frame available flag.  Don't need to disable interrupts (I
-    // think).
-    return _frame_available;
-}
-
-/**
- * This function will copy the frame from the internal buffer to the supplied
- * buffer.  If there is no frame available, then no copy is performed and -1
- * is returned.  Otherwise, this function will copy up to buffer_size entries
- * to the supplied pointer.
- *
- * dest_buffer:
- * frame_size: This is the number of entries in the destination buffer, NOT
- * the number of bytes in the dest buffer.
- */
-int16_t BTHI_IR_Decoder::copyFrame(ir_decoder_edge_t *dest_buffer, uint16_t buffer_size) {
-    if (0 == _frame_available) {
-       // TODO: Make E_NOT_OK
-       return -1;
-    }
-    
-    // TODO: Have to decide what to do if we know we overran the internal
-    // buffer bounds.  I think we should return a special error code.
-
-    // Have to disable interrupts at this point to avoid having the latched
-    // frame data overwritten during the copy.
+void IR_BufferingStreamDecoder::setSegmentBuffer(ir_segment_t *segments, 
+        uint8_t num_segments) {
     cli();
-
-    // TODO: consider typdefing uint16 that has to accomodate the largest
-    // buffer size.
-    // Copy only up to the minimum of the supplied buffer size and the size of
-    // the frame in the internal buffer.
-    uint16_t num_samples = min(buffer_size, _latched_frame_length);
-    memcpy(dest_buffer, _latched_frame_buffer, 
-            num_samples * sizeof(_latched_frame_buffer[0]));
-
-    // Put down the flag
-    _frame_available = 0;
-
-    // Re-enable interrupts
+    _segments = segments;
+    _max_segments = num_segments;
+    _count = 0;
+    _frame_complete = 0;
+    _segment_overflows = 0;
+    _first_edge = 1;
     sei();
 }
 
-BTHI_IR_Decoder IR_Decoder;
+void IR_BufferingStreamDecoder::receiveNextFrame(void) {
+    cli();
+    _count = 0;
+    _frame_complete = 0;
+    _segment_overflows = 0;
+    _first_edge = 1;
+    sei();
+}
+
+uint8_t IR_BufferingStreamDecoder::isDone(void) {
+    return _frame_complete;
+}
+
+uint8_t IR_BufferingStreamDecoder::getSegmentCount(void) {
+    return _count;
+}
+
+uint8_t IR_BufferingStreamDecoder::getSegmentOverflowCount(void) {
+    return _segment_overflows;
+}
+
+uint8_t IR_BufferingStreamDecoder::clearSegmentOverflowCount(void) {
+    uint8_t tmp = _segment_overflows;
+    _segment_overflows = 0;
+    return tmp;
+}
 
 // Register interrupt handler
-ISR(PCINT2_vect) {
-  IR_Decoder.interrupt();
+ISR(TIMER1_CAPT_vect) {
+    IR_InputCaptureInterface.captureInterrupt();
+}
+
+ISR(TIMER1_OVF_vect) {
+    IR_InputCaptureInterface.overflowInterrupt();
 }
 
--- examples/BTHI_IR_Decoder_Example/BTHI_IR_Decoder_Example.ino
+++ examples/BTHI_IR_Decoder_Example/BTHI_IR_Decoder_Example.ino
@@ -1,21 +1,38 @@
+/*----------------------------------------------------------------------------------
+ * Example for the Universal IR decoding library.
+ *
+ * This example uses a buffered decoding strategy. When a frame is seen, it's 
+ * printed out and then the next frame can be received.
+ */
 #include <BTHI_IR_Decoder.h>
 
+IR_BufferingStreamDecoder decoder;
+
+// Room for 128 segments.  Most remotes that we've tested with have only about 
+// 60-70 segments in their commands 
+#define NUM_SEGMENTS  128
+
+ir_segment_t g_segment_buffer[NUM_SEGMENTS];
+
 void setup() {
-    IR_Decoder.setup();
     Serial.begin(115200);
-    Serial.println("Waiting for Frames:");
+    Serial.println("\n--- BTHI IR Decoding Example ---\n");
+    
+    // Set up the decoder first. Give it a buffer to store the edges in 
+    decoder.setSegmentBuffer(g_segment_buffer, NUM_SEGMENTS);
+    // Use Pin 8 (the input capture pin on the UNO)
+    IR_InputCaptureInterface.setup(&decoder, 8, IR_POLARITY_AUTO);
 }
 
 void loop() {
-    int16_t res;
-    ir_decoder_edge_t frame_buffer[64];
-
-    if (IR_Decoder.isFrameAvailable()) {
-        res = IR_Decoder.copyFrame(&frame_buffer[0], sizeof(frame_buffer));
-        if (res > 0) {
-            // Process the frame
-            Serial.println("Frame Received!");
-        }
+  
+    if (decoder.isDone()) {
+      Serial.println("Frame Received!");
+      decoder.debugPrintFrame();
+      
+      // This will allow the decoder to accept another frame 
+      // (overwriting the one we just printed out)
+      decoder.receiveNextFrame();
     }
 }
 
